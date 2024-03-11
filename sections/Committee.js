7e8bcfc405ace7582b3b202eed5052720e1b05c7
Adam Przybylek <adam.przybylek@gmail.com>
2024-03-11 09:40:03 +0100

    Program Commitee added

--- sections/Committee.js
+++ sections/Committee.js
@@ -2,7 +2,6 @@ import Container from 'react-bootstrap/Container'
 import PeopleList from '../components/PeopleList'
 
 
-/*
 const Commitee = ({ people }) => (
   <Container className="bg-white section p-4 my-5 rounded">
     <a className='target' id='committee'/>
@@ -10,9 +9,9 @@ const Commitee = ({ people }) => (
     <PeopleList people={people} />
   </Container>
 )
-*/
 
 
+/*
 const Commitee = () => (
   <Container className="bg-white section p-4 my-5 rounded">
     <a className='target' id='committee'/>
@@ -20,5 +19,6 @@ const Commitee = () => (
     <h3>TBA...</h3>
   </Container>
 )
+*/
 
 export default Commitee
