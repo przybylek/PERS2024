6e78d84da07c410851f22c6c8c6717b9af18b281
Adam Przybylek <adam.przybylek@gmail.com>
2024-07-30 18:25:31 +0200

    Time fixed

--- sections/Program.js
+++ sections/Program.js
@@ -16,12 +16,12 @@ const Program = () => (
     <Table bordered className="mt-5">
       <tbody>	
 
-        <tr className='text-center table-primary'><th>Wednesday, August 28th</th><th>session chair: Justyna Głogowska</th></tr>
+        <tr className='text-center table-primary'><th>Wednesday, August 28th</th><th>session chair: <em>Justyna Głogowska</em></th></tr>
 
         <tr><td className='text-nowrap'>16:00 - 16:15</td><td>Development of Collaborative Business Intelligence Framework for Tourism Analysis (<em>Olga Cherednichenko and Oleksandr Sutiahin</em>)</td></tr>
         <tr><td className='text-nowrap'>16:15 - 16:30</td><td>Session-based Recommendation with Graph Neural Networks with an Examination of the Impact of Local and Global Vectors (<em>Justyna Głogowska, Dariusz Kobiela and Szymon Mielewczyk</em>)</td></tr>
         <tr><td className='text-nowrap'>16:30 - 16:45</td><td>Senselife: Service Recommendation and Frailty Prevention through Knowledge Models (<em>Ghassen Frikha, Xavier Lorca, Pingaud Herve, Adel Taweel, Christophe Bortolaso, Katarzyna Borgiel and Elyes Lamine</em>)</td></tr>        
-        <tr><td className='text-nowrap'>16:30 - 16:45</td><td>Evaluating Diversity in Sequential Group Recommendations (<em>Haider Zulfiqar, Emilia Lenzi and Kostas Stefanidis</em>)</td></tr>        
+        <tr><td className='text-nowrap'>16:45 - 17:00</td><td>Evaluating Diversity in Sequential Group Recommendations (<em>Haider Zulfiqar, Emilia Lenzi and Kostas Stefanidis</em>)</td></tr>        
 
       </tbody>
     </Table>
